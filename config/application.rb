c857bcbf54fbc13251410eb49e29983e0be846fb
naconaco175 <nikoniko73.0107@gmail.com>
2021-03-21 07:06:45 +0900

    日本語化対応#7

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.generators do |g|
       g.test_framework :rspec,
