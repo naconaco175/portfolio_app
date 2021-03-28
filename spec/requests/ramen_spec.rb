40fa759e623db449dbdf170cd91bda84cb505ee6
naconaco175 <nikoniko73.0107@gmail.com>
2021-03-28 11:21:52 +0900

    request_specの追加

--- spec/requests/ramen_spec.rb
+++ spec/requests/ramen_spec.rb
@@ -1,18 +1,115 @@
 require 'rails_helper'
 
 RSpec.describe "Ramens", type: :request do
-  # describe "GET /index" do
-  #   it "returns http success" do
-  #     get "/users/index"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET /index" do
+    it "リクエストが成功すること" do
+      get
+      expect(response).to have_http_status(:success)
+    end
 
-  # describe "GET /show" do
-  #   it "returns http success" do
-  #     get "/users/show"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    it '投稿が表示されていること' do
+    end
+  end
+
+  describe "GET /show" do
+    it "リクエストが成功すること" do
+      get
+      expect(response).to have_http_status(:success)
+    end
+
+    it '投稿が表示されていること' do
+    end
+
+    it '投稿が存在しない場合' do
+    end
+  end
+
+  describe "GET /new" do
+    it "リクエストが成功すること" do
+      get
+      expect(response).to have_http_status(:success)
+    end
+
+
+  end
+
+  describe "GET /edit" do
+    it "リクエストが成功すること" do
+      get
+      expect(response).to have_http_status(:success)
+    end
+
+    it '投稿が表示されていること' do
+    end
+  end
+
+  describe "GET /create" do
+    context 'パラメータが妥当な場合' do
+      it "リクエストが成功すること" do
+        get
+        expect(response).to have_http_status(:success)
+      end
+
+      it '投稿が保存されていること' do
+      end
+
+      it 'リダイレクトすること' do
+      end
+    end
+
+    context 'パラメータが不正な場合' do
+      it "リクエストが成功すること" do
+        get
+        expect(response).to have_http_status(:success)
+      end
+
+      it '投稿が保存されないこと' do
+      end
+
+      it 'エラーが出ること' do
+      end
+    end
+  end
+
+  describe "GET /update" do
+    context 'パラメータが妥当な場合' do
+      it "リクエストが成功すること" do
+        get
+        expect(response).to have_http_status(:success)
+      end
+
+      it '投稿が更新されていること' do
+      end
+
+      it 'リダイレクトすること' do
+      end
+    end
+
+    context 'パラメータが不正な場合' do
+      it "リクエストが成功すること" do
+        get
+        expect(response).to have_http_status(:success)
+      end
+
+      it '投稿が更新されないこと' do
+      end
+
+      it 'エラーが出ること' do
+      end
+    end
+  end
+
+  describe "GET /destory" do
+    it "リクエストが成功すること" do
+      get
+      expect(response).to have_http_status(:success)
+    end
+
+    it '投稿が削除されること' do
+    end
+
+    it 'ユーザー一覧にリダイレクトすること' do
+    end
+  end
 
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  # describe "GET /index" do
-  #   it "returns http success" do
-  #     get "/users/index"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET /index" do
+    before do
+      user = create_list(:user, 11)
+      next_page_user = create(:user, name: "next_page_user")
+      get users_path
+    end
 
-  # describe "GET /show" do
-  #   it "returns http success" do
-  #     get "/users/show"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    it "リクエストが成功すること" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "ユーザーが10人表示される" do
+      expect(response.body).not_to include "next_page_user"
+    end
+  end
+
+  describe "GET /show" do
+    before do
+      user = create(:user, name: "test1")
+      get user_path(user)
+    end
+
+    it "リクエストが成功すること" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "ユーザーが表示されること" do
+      expect(response.body).to include "test1"
+    end
+  end
 
 end
