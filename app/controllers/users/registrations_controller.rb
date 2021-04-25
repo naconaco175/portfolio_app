ec3a02cadb1c2b9527582298c01b54766428083f
naconaco175 <nikoniko73.0107@gmail.com>
2021-04-25 22:06:38 +0900

    テストログイン機能の追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,40 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, only: [:create, :update]
-
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-
-  # POST /resource
-  # def create
-  #   super
-  # end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  before_action :ensure_normal_user, only: [:destroy, :update]
 
   protected
 
@@ -47,5 +14,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,        keys: [:name, :profile, %i(avatar)])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, %i(avatar)])
+    end
+
+    def ensure_normal_user
+      if resource.email == 'guest@example.com'
+        redirect_to root_path, alert: 'ゲストユーザーは更新・削除できません。'
+      end
     end
 end
